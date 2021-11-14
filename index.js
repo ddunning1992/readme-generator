27e1721859fa029406a936e4a9ff390ed6fe1044
Dunnhalr <88796836+ddunning1992@users.noreply.github.com>
2021-11-14 01:37:47 -0600

    set up sequence of prompts through inquirer and pass the data to markdown generation functionality

--- index.js
+++ index.js
@@ -1,13 +1,120 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = projectData => {
 
+    if (!projectData) {
+        projectData = [];
+    }
+
+    return inquirer.prompt([
+
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your application? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your application name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a description of your application. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an application description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Describe how to install the application. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please give the install directions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Describe how to use the application (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please describe how to use the application!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Give instructions for how to contribute to the project.',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Give instructions for testing the application.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license badge would you like to inlude?',
+            choices: ['MIT', 'GPLv3', 'GPL', 'none'],
+            default: 'none'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please provide a link to your GitHub profile. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a link to your GitHub profile!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide your email address. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your email!');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(projectAnswers => {
+        projectData.push(projectAnswers);
+        console.log(projectData);
+        return projectData;
+    })
+};
+
+promptUser()
+    .then(projectData => {
+        return generateMarkdown(projectData);
+    })
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+//function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,8 +11,48 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+const generateContribution = contributionText => {
+  if (!contributionText) {
+    return '';
+  }
+
+  return `
+  ## How to Contribute
+  ${data.contribution}
+  `
+}
+
+const generateTesting = testingText => {
+  if (!testingText)
+  return '';
+}
+
+return `
+## Testing Instructions
+${data.testing}
+`
+
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Installation Instructions
+  ${data.install}
+
+  ## Usage Instructions
+  ${data.usage}
+
+  ${generateContribution(contribution)}
+
+  ${generateTesting(testing)}
+
+  ## Questions
+  If you have additional questions, contact me at:
+  GitHub: ${data.github}
+  Email: ${data.email}
+
 
 `;
 }
